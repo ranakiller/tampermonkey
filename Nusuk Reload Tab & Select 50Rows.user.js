29a17ddaeb34a544d0d31709f665f19c06f16085
ranakiller <86097544+ranakiller@users.noreply.github.com>
2024-08-20 19:06:29 +0500

    Update Nusuk Reload Tab & Select 50Rows.user.js

--- Nusuk Reload Tab & Select 50Rows.user.js
+++ Nusuk Reload Tab & Select 50Rows.user.js
@@ -276,6 +276,102 @@
 })();
 
 
+// Clicking Add Mutamer Button again&again on feedlig list page
+(function() {
+    'use strict';
+
+    function clickAddMuatamerLink() {
+        const addMuatamerLink = document.querySelector('a.btn.btn-outline-black1.ml-auto[href*="/bsp/ExternalAgencies/Groups/createMutamerIntoGroup/"]');
+        if (addMuatamerLink) {
+            addMuatamerLink.click();
+        }
+    }
+    clickAddMuatamerLink();
+
+    // Function to Change heading to Add Mutamer Link in Mutamer list page
+    function addMtmrLnk() {
+        const MtmrLstElement = document.querySelector('.kt-portlet__head-label h3.kt-portlet__head-title');
+        if (MtmrLstElement) {
+            const xxURL = window.location.href;
+            const newURL = xxURL.replace('GetMuatamerListDetails', 'createMutamerIntoGroup');
+            MtmrLstElement.innerHTML = `<a href="${newURL}">Add</a>`;
+        }
+    }
+    window.addEventListener('load', addMtmrLnk);
+})();
+
+
+// Function to extract PPNo and PaxName from Mutamer List and put in text area at the bottom
+(function() {
+    'use strict';
+
+    let isTextAreaAdded = false;
+
+    function addTextArea() {
+        if (isTextAreaAdded) return;
+
+        const wrapper = document.querySelector('#MuatamerList_wrapper');
+        if (!wrapper) return;
+
+        const newDiv = document.createElement('div');
+        newDiv.style.marginTop = '20px';
+
+        const textArea = document.createElement('textarea');
+        textArea.id = 'tableDataTextArea';
+        textArea.style.width = '100%';
+        textArea.style.height = '50px';
+        textArea.setAttribute('readonly', true);
+
+        newDiv.appendChild(textArea);
+        wrapper.parentNode.insertBefore(newDiv, wrapper.nextSibling);
+
+        isTextAreaAdded = true;
+    }
+
+    function extractTableData() {
+        const table = document.querySelector('#MuatamerList');
+
+        if (!table) return;
+
+        const rows = table.querySelectorAll('tbody tr');
+        if (rows.length === 0) return;
+
+        const rowData = Array.from(rows).map((row, index) => {
+            const cells = Array.from(row.querySelectorAll('td')).map(td => td.innerText.trim());
+            const fifthValue = cells[5] || '';
+            const thirdValue = cells[3] || '';
+            return `${fifthValue}\t${thirdValue}`;
+            // return `${index + 1}. ${fifthValue}\t${thirdValue}`;
+        }).join('\n');
+
+        const textArea = document.querySelector('#tableDataTextArea');
+        if (textArea) {
+            textArea.value = rowData;
+        }
+    }
+
+    function observeDOM() {
+        const observer = new MutationObserver(() => {
+            if (!isTextAreaAdded) addTextArea();
+            extractTableData();
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+        if (isTextAreaAdded) observer.disconnect();
+    }
+
+    observeDOM();
+    document.addEventListener('DOMContentLoaded', () => {
+        addTextArea();
+        extractTableData();
+    });
+})();
+
+
 // Function to extract Group Code,Name And Pax Count from Groups Management List and put in text area at the bottom
 (function() {
     'use strict';
